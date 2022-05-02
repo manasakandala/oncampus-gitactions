615474a2b64cb2896375216c0daf38f6a54988e4
manasakandala <92063868+manasakandala@users.noreply.github.com>
2022-05-02 14:33:20 -0500

    some changes

--- hello_world/app.py
+++ hello_world/app.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello World Program!!",
+            "message": "Hello There!",
             # "location": ip.text.replace("\n", "")
         }),
     }
