a983b297ddcfb3afe4363c9d3008ca40800ae8a5
manasakandala <92063868+manasakandala@users.noreply.github.com>
2022-05-02 12:01:34 -0500

    access3 check

--- hello_world/app.py
+++ hello_world/app.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "CloudFrontFullAccess removed!",
+            "message": "AmazonAPIGatewayAdministrator removed!",
             # "location": ip.text.replace("\n", "")
         }),
     }
