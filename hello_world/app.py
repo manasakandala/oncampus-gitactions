39506a2137356933f0131c2ea16909d36b46c069
manasakandala <92063868+manasakandala@users.noreply.github.com>
2022-05-02 12:19:26 -0500

    access7 check

--- hello_world/app.py
+++ hello_world/app.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "AWSLambdaExecute removed!",
+            "message": "nothing removed!",
             # "location": ip.text.replace("\n", "")
         }),
     }
