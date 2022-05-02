436f97814c8a41e81227cd9675513e931de85fdf
manasakandala <92063868+manasakandala@users.noreply.github.com>
2022-05-02 12:27:37 -0500

    access9 check

--- hello_world/app.py
+++ hello_world/app.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "AWSCloudFormationFullAccess removed!",
+            "message": "AWSLambda_FullAccess removed!",
             # "location": ip.text.replace("\n", "")
         }),
     }
